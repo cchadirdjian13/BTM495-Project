1c1422ea7386f3699fe754188969b7aea99c9e14
Christopher Chadirdjian <christopher.chadirdjian@polymtl.ca>
2026-04-10 18:24:24 -0400

    Removed duplicate files

--- Class Diagram/service.py
+++
@@ -1,6 +0,0 @@
-class Service:
-    def __init__(self, service_id, name, duration, price):
-        self.service_id = service_id
-        self.name = name
-        self.duration = duration
-        self.price = price
